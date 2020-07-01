55e907623005c20752916f90988b942f162ba70e
Jeremy Marino <jermarino16@gmail.com>
2020-07-01 08:18:10 -0700

    added logo clickable to app bar

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import AppBar from "@material-ui/core/AppBar";
@@ -30,7 +30,13 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: "3em",
   },
   logo: {
-    height: "7em",
+    height: "8em",
+  },
+  logoContainer: {
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
   },
   tabContainer: {
     marginLeft: "auto",
@@ -58,12 +64,38 @@ const Header = () => {
     setValue(newValue);
   };
 
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === "/about" && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === "/contact" && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5);
+    }
+  }, [value]);
+
   return (
     <React.Fragment>
       <ElevationScroll>
         <AppBar>
           <Toolbar disableGutters>
-            <img alt="company logo" className={classes.logo} src={logo} />
+            <Button
+              component={Link}
+              to="/"
+              disableRipple
+              className={classes.logoContainer}
+              onClick={() => {
+                setValue(0);
+              }}
+            >
+              <img alt="company logo" className={classes.logo} src={logo} />
+            </Button>
             <Tabs
               className={classes.tabContainer}
               value={value}
