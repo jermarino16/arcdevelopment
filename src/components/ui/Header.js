d99eebdc1921a5cf1eec975f801fc92a05a13561
Jeremy Marino <jermarino16@gmail.com>
2020-07-01 07:35:09 -0700

    craeted and styled tabs

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,6 +3,8 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import { makeStyles } from "@material-ui/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 import logo from "../../assets/logo.svg";
 
@@ -27,6 +29,14 @@ const useStyles = makeStyles((theme) => ({
   logo: {
     height: "7em",
   },
+  tabContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px",
+  },
 }));
 
 const Header = () => {
@@ -38,6 +48,13 @@ const Header = () => {
         <AppBar>
           <Toolbar disableGutters>
             <img alt="company logo" className={classes.logo} src={logo} />
+            <Tabs className={classes.tabContainer} value={1}>
+              <Tab className={classes.tab} label="Home" />
+              <Tab className={classes.tab} label="Services" />
+              <Tab className={classes.tab} label="Revolution" />
+              <Tab className={classes.tab} label="About Us" />
+              <Tab className={classes.tab} label="Contact Us" />
+            </Tabs>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
